cbb2ef29a00e90499aedce37c3d99b560d4f2d7f
jominjae1006 <jmjae8240@gmail.com>
2026-03-27 14:23:04 +0900

    스캐너 논리연산

--- src/main/java/Code03_12.java
+++ src/main/java/Code03_12.java
@@ -1,9 +1,15 @@
-public class Code03_12 {
-    public static void main(String[] args){
-        int num = 99;
+import java.util.Scanner;
 
-        System.out.println((num > 100) && ( num < 200));
+public class Code03_12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+
+
+        System.out.println((num > 100) && (num < 200));
         System.out.println((num == 99) || (num == 100));
         System.out.println(!(num == 100));
+        sc.close();
     }
 }
