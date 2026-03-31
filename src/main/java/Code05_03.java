80a1b307d1f6ce1804fda6d7bfebae20228feada
jominjae1006 <jmjae8240@gmail.com>
2026-03-31 10:23:45 +0900

    if 와 else

---
+++ src/main/java/Code05_03.java
@@ -0,0 +1,12 @@
+public class Code05_03 {
+    public static void main(String[] args){
+        int a = 99;
+
+        if (a < 100){
+            System.out.println("100보다 작군요.");
+        }
+        else {
+            System.out.println("100보다 크군요.");
+        }
+    }
+}
