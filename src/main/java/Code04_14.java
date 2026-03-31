701efdb7d315c23dcdb746c72905babdb4e5f6b5
jominjae1006 <jmjae8240@gmail.com>
2026-03-31 09:24:54 +0900

    indexOf 활용

---
+++ src/main/java/Code04_14.java
@@ -0,0 +1,8 @@
+public class Code04_14 {
+    public static void main(String[] args){
+        String a = "난생처음 자바";
+
+        System.out.println(a.indexOf("난생"));
+        System.out.println(a.indexOf("처"));
+    }
+}
