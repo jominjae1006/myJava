13f3951cb083abd67b12539b341c189534145126
jominjae1006 <jmjae8240@gmail.com>
2026-04-10 14:24:57 +0900

    for문 + 누적 변수 사용

---
+++ src/main/java/Code06_07.java
@@ -0,0 +1,9 @@
+public class Code06_07 {
+    public static void main(String[] args){
+        int hap=0;
+        for(int a=1; a<=1000; a++){
+            hap +=a;
+        }
+        System.out.println("1부터 10000까지 의 합계 :" + hap);
+    }
+}
