9800b96586e246fe87b4ff77254055bdcd2f69c6
jominjae1006 <jmjae8240@gmail.com>
2026-04-10 15:01:37 +0900

    while문

---
+++ src/main/java/Code06_10.java
@@ -0,0 +1,9 @@
+public class Code06_10 {
+    public static void main(String[] args){
+        int a=0;
+        while(a<3){
+            System.out.println(a+": 난생처음 자바는 재미있습니다. ^^");
+            a++;
+        }
+    }
+}
