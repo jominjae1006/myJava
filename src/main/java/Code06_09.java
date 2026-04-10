757523315cfc418243b519920e24dcfbe2ae5fde
jominjae1006 <jmjae8240@gmail.com>
2026-04-10 14:33:04 +0900

    중첩 for 문

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,10 @@
+public class Code06_09 {
+    public static void main(String[] args){
+
+        for(int a=0; a<3; a++){
+            for(int b=0; b<2; b++){
+                System.out.println("난생처음은 쉽습니다.^^"+"(a:"+a+",b:"+b+")");
+            }
+        }
+    }
+}
