a82c3a79e3ce6bea001db32f813b58d4b9678e86
jominjae1006 <jmjae8240@gmail.com>
2026-04-10 15:22:10 +0900

    continue 실행 흐름

---
+++ src/main/java/Code06_15.java
@@ -0,0 +1,11 @@
+public class Code06_15 {
+    public static void main(String[] args){
+        int hap=0;
+        for(int a=1; a<=100; a++){
+            if(a%4 !=0);
+            hap +=a;
+        }
+        System.out.println("1~100의 합계(4의 배수 제외) :"+ hap);
+    }
+
+}
