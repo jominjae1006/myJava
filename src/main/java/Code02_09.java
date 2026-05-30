7bee37d3627171b0ef1eac9074483958e0fa7e10
jominjae1006 <jmjae8240@gmail.com>
2026-05-30 12:29:34 +0900

    plus() 메서드 예제

--- src/main/java/Code02_09.java
+++ src/main/java/Code02_09.java
@@ -2,17 +2,17 @@ public class Code02_09 {
     public static void main(String[] args) {
 
         int number1 = 10, number2 = 20;
-        int resAdd;
+        int resBdd;
         int resSub;
         int resMul;
-        int resDiv;
+        double resDiv;
 
-        resAdd = number1 + number2;
+        resBdd = number1 + number2;
         resSub = number1 - number2;
         resMul = number1 * number2;
-        resDiv = number1 / number2;
+        resDiv = (double) number1 / number2;
 
-        System.out.println(number1 + "+" + number2 + "=" + resAdd);
+        System.out.println(number1 + "+" + number2 + "=" + resBdd);
         System.out.println(number1 + "-" + number2 + "=" + resSub);
         System.out.println(number1 + "*" + number2 + "=" + resMul);
         System.out.println(number1 + "/" + number2 + "=" + resDiv);
